c7932d591cc9ba5f8584ba8f462722596b547ebf
Stringmander <nrusso@protonmail.com>
2020-10-12 15:59:49 -0400

    extract model logic into generic class Model

--- src/models/ApiSync.ts
+++ src/models/ApiSync.ts
@@ -4,7 +4,7 @@ interface HasId {
   id?: number;
 }
 
-export class Sync<T extends HasId> {
+export class ApiSync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   public async fetch(id: number): Promise<AxiosPromise> {
---
+++ src/models/Model.ts
@@ -0,0 +1,60 @@
+import { AxiosPromise, AxiosResponse } from "axios";
+
+interface ModelAttributes<T> {
+  get<K extends keyof T>(key: K): T[K];
+  getAll(): T;
+  set(value: T): void;
+}
+
+interface Sync<T> {
+  fetch(id: number): Promise<AxiosPromise>;
+  save(data: T): Promise<AxiosPromise>
+}
+
+interface Events {
+  on(eventName: string, callback: () => void): void;
+  trigger(evenName: string): void;
+}
+
+interface HasId {
+  id?: number;
+}
+
+export class Model<T extends HasId> {
+  constructor(
+    private attributes: ModelAttributes<T>,
+    private events: Events,
+    private sync: Sync<T>
+  ) {}
+
+  on = this.events.on;
+  trigger = this.events.trigger;
+  get = this.attributes.get;
+
+  set(update: T): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id')
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id')
+    }
+
+    this.sync.fetch(id).then(async (response: Promise<AxiosResponse>): Promise<void> => {
+      this.set((await response).data)
+    });
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: Promise<AxiosResponse>): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error')
+      });
+  }
+}
