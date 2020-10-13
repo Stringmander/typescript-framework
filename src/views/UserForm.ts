d3abede5772f21df47d9683381bff2ecebc16242
Stringmander <nrusso@protonmail.com>
2020-10-13 15:09:40 -0400

    add save button to UserForm enabling post request via UI

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,13 +5,18 @@ export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void }  {
     return {
       'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetNameClick
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
     };
   }
 
+  onSaveClick = (): void => {
+    this.model.save()
+  };
+
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
-  }
+  };
 
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
@@ -21,17 +26,15 @@ export class UserForm extends View<User, UserProps> {
 
       this.model.set({ name });
     }
-  }
+  };
 
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>User name: ${this.model.get('name')}</div>
-        <div>User age: ${this.model.get('age')}</div>
-        <input />
+        <input  placeholder="${this.model.get('name')}" />
         <button class="set-name" >Change Name</button>
         <button class="set-age" >Set Random Age</button>
+        <button class="save-model" >Save User</button>
       </div>
     `
   }
