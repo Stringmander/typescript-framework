1463eff923632ea1c2c271c744d55590839ad78b
Stringmander <nrusso@protonmail.com>
2020-10-14 13:31:38 -0400

    create CollectionView & UserList to display list of users

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
+import { User, UserProps } from './models/User';
 import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
 
 const user = User.buildUser({ name: 'NAME', age: 20})
+let users = User.buildUserCollection();
 
-const root = document.getElementById('root');
+const edit = document.getElementById('edit');
+const list = document.getElementById('list');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-
-  userEdit.render();
-
-  console.log(userEdit);
+if (edit) {
+  new UserEdit(edit, user).render();
 } else {
   throw new Error('Root element not found')
 }
+
+users.on('change', () => {
+  if (list) {
+    new UserList(list, users).render();
+  }
+});
+
+users.fetch();
+
+user.on('save', () => {
+  users.models = [];
+  users.fetch();
+});
